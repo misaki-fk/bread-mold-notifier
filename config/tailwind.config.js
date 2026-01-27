19a4ccd13068f8058b55daf0cf968408d78b9054
深山海咲 <fukayama.misaki@gmail.com>
2026-01-27 14:23:30 +0900

    DaisyUI導入とUIの調整

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,10 +2,10 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './public/*.html',
+    './app/views/**/*.{erb,html,slim}',
     './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/javascript/**/*.{js,ts}',
+    './app/assets/stylesheets/**/*.css',
   ],
   theme: {
     extend: {
@@ -15,8 +15,35 @@ module.exports = {
     },
   },
   plugins: [
-    // require('@tailwindcss/forms'),
-    // require('@tailwindcss/typography'),
-    // require('@tailwindcss/container-queries'),
-  ]
+    require("daisyui"),
+  ],
+  daisyui: {
+    themes: [
+      {
+        bread: {
+          "primary": "#a56c2fff",
+          "primary-content": "#ffffff",
+
+          "secondary": "#e9c455ff",
+          "secondary-content": "#4B3425",
+
+          "accent": "#E17055",
+          "accent-content": "#ffffff",
+
+          "neutral": "#4B3425",
+          "neutral-content": "#ffffff",
+
+          "base-100": "#FFF8EE",
+          "base-200": "#F5EBDD",
+          "base-300": "#EADCC8",
+          "base-content": "#4B3425",
+
+          "info": "#A7C7E7",
+          "success": "#6FB98F",
+          "warning": "#F4B266",
+          "error": "#E06666",
+        },
+      },
+    ],
+  },
 }
