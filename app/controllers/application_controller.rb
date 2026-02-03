fee4b42cde364f9dd0b5efe552f62a58675231e5
深山海咲 <fukayama.misaki@gmail.com>
2026-02-03 22:02:56 +0900

    ログアウト後の遷移画面を設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,9 @@ class ApplicationController < ActionController::Base
   def after_sign_up_path_for(resource)
     home_path
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+  
 end
