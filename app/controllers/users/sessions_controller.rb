187ca186e42cf6dab711c90415ebf9656899e6f3
深山海咲 <fukayama.misaki@gmail.com>
2026-02-08 14:26:36 +0900

    guestを削除

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,11 +1,3 @@
 # app/controllers/users/sessions_controller.rb
 class Users::SessionsController < Devise::SessionsController
-  def guest
-    user = User.find_or_create_by!(email: 'guest@example.com') do |u|
-      u.password = SecureRandom.urlsafe_base64
-    end
-
-    sign_in user
-    redirect_to home_path, notice: 'ゲストとしてログインしました'
-  end
 end
