95490766e46b8efbcde468f0b86475c92e7c733e
深山海咲 <fukayama.misaki@gmail.com>
2026-03-03 22:04:14 +0900

    feat:LINEでシェア機能

--- app/helpers/breads_helper.rb
+++ app/helpers/breads_helper.rb
@@ -1,2 +1,16 @@
 module BreadsHelper
+  def line_share_url(bread)
+    remaining = bread.remaining_count || 0
+    share_text = <<~TEXT
+    🏠【家のパンの情報】🏠
+
+    🍞 #{bread.bread_type.name}
+    ⏰ 消費期限: #{bread.expiration_date.strftime('%Y.%m.%d')}
+    ⭐️ 残り枚数: #{remaining}枚
+    TEXT
+
+    encoded_text = ERB::Util.url_encode(share_text)
+
+    "https://social-plugins.line.me/lineit/share?text=#{encoded_text}"
+  end
 end
