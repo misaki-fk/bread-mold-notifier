5d55a42af912daae89bfc289e2c1bd5f478a017c
深山海咲 <fukayama.misaki@gmail.com>
2026-03-07 21:44:39 +0900

    feat:resend api方式のリセットメール送信

---
+++ app/mailers/devise_mailer.rb
@@ -0,0 +1,12 @@
+class DeviseMailer < Devise::Mailer
+  def reset_password_instructions(record, token, opts = {})
+    reset_url = edit_password_url(record, reset_password_token: token)
+
+    Resend::Emails.send({
+      from: "Pankabi <noreply@pankabi.app>",
+      to: [record.email],
+      subject: "【Pankabi】パスワード再設定",
+      html: "<p>パスワード再設定はこちら</p><a href='#{reset_url}'>#{reset_url}</a>"
+    })
+  end
+end
