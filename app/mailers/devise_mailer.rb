3614e856573f178b76475e490292583cb3c5660d
深山海咲 <fukayama.misaki@gmail.com>
2026-03-07 22:29:02 +0900

    fix:URL helper

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,6 +1,6 @@
 class DeviseMailer < Devise::Mailer
   def reset_password_instructions(record, token, opts = {})
-    reset_url = edit_password_url(record, reset_password_token: token)
+    reset_url = edit__user_password_url(record, reset_password_token: token)
 
     Resend::Emails.send({
       from: "Pankabi <noreply@pankabi.app>",
