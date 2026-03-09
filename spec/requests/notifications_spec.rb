aabde2e714bafa936df0b6f02753c10e42a49bb5
深山海咲 <fukayama.misaki@gmail.com>
2026-03-09 21:50:41 +0900

    add:notifications_spec.rb

--- spec/requests/notifications_spec.rb
+++ spec/requests/notifications_spec.rb
@@ -1,11 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "Notifications", type: :request do
+  include Devise::Test::IntegrationHelpers
+
+  let(:user) { create(:user) }
+
   describe "GET /index" do
-    it "returns http success" do
-      get "/notifications/index"
-      expect(response).to have_http_status(:success)
+    context "ログインしている場合" do
+      before do
+        sign_in user
+      end
+
+      it "returns success" do
+        get notifications_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "未ログインの場合" do
+      it "ログインページにリダイレクトされる" do
+        get notifications_path
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
-
 end
