449ee0e871a9624890ebe0f449321a2215a6cce8
深山海咲 <fukayama.misaki@gmail.com>
2026-04-03 23:02:34 +0900

    fix:typo

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -5,7 +5,7 @@ class DeviseMailer < Devise::Mailer
     reset_url = edit_user_password_url(record, reset_password_token: token)
 
     Resend::Emails.send({
-      from: "Pankabi <"no-reply@pankabi.com>",
+      from: "Pankabi <no-reply@pankabi.com>",
       to: [record.email],
       subject: "【Pankabi】パスワード再設定",
       html: "<p>パスワード再設定はこちら</p><a href='#{reset_url}'>#{reset_url}</a>"
