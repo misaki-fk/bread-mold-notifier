81d347a2e3d08a1e0766631469b4b217600f1844
深山海咲 <fukayama.misaki@gmail.com>
2026-04-10 20:34:21 +0900

    fix: sitemap生成時のDB依存を削除（ビルドエラー対策）

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -4,9 +4,4 @@ SitemapGenerator::Sitemap.create do
   add root_path
   add breads_path
 
-  # パン詳細ページ
-  Bread.find_each do |bread|
-    add bread_path(bread), lastmod: bread.updated_at
-  end
-
 end
