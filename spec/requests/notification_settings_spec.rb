4d65a833846f6ebd85c97a7b3c93c73b9af26800
深山海咲 <fukayama.misaki@gmail.com>
2026-04-21 14:20:44 +0900

    chore:edit,updateは使用していないので削除

--- spec/requests/notification_settings_spec.rb
+++ spec/requests/notification_settings_spec.rb
@@ -7,19 +7,4 @@ RSpec.describe "NotificationSettings", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/notification_settings/edit"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /update" do
-    it "returns http success" do
-      get "/notification_settings/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
