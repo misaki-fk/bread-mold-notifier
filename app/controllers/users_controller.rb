05ec06f88499c5eb07646065b6bb8a099fa0001c
深山海咲 <fukayama.misaki@gmail.com>
2026-04-27 09:36:24 +0900

    fix:redirect_toの位置修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,11 +19,11 @@ class UsersController < ApplicationController
           notification_type: "system",
           message: "LINE通知機能が追加されました！\n設定一覧からLINE通知を受け取る設定ができます。",
         )
+      end
 
         redirect_to home_path
-      else
-        render :new
-      end
+    else
+      render :new
     end
   end
 
