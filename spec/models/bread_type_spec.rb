ea53f2189f085eb70bf04f9ef4df19a83a44b3ad
深山海咲 <fukayama.misaki@gmail.com>
2026-05-13 16:41:17 +0900

    test:add BreadType バリデーション spec

--- spec/models/bread_type_spec.rb
+++ spec/models/bread_type_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe BreadType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーショnン' do
+    context 'nameが空の時' do
+      it '無効であること' do
+        bread_type = BreadType.new(name: nil)
+        expect(bread_type).to be_invalid
+      end
+    end
+    context 'nameが入っている時' do
+      it '有効であること' do
+        bread_type = BreadType.new(name: '食パン')
+        expect(bread_type).to be_valid
+      end
+    end
+  end
 end
