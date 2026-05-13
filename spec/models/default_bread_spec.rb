53cae5b0f992d34c68960adb13f4949a0e7b0f44
深山海咲 <fukayama.misaki@gmail.com>
2026-05-13 17:53:12 +0900

    test:add Default_bread 関連spec

--- spec/models/default_bread_spec.rb
+++ spec/models/default_bread_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe DefaultBread, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '関連' do
+    context 'userがnilの時' do
+      it '無効であること' do
+        default_bread = build(:default_bread, user: nil)
+        expect(default_bread).to be_invalid
+      end
+    end
+
+    context 'bread_typeがnilの時' do
+      it '無効であること' do
+        default_bread = build(:default_bread, bread_type: nil)
+        expect(default_bread).to be_invalid
+      end
+    end
+
+    context 'userとbread_typeが両方ある時' do
+      it '有効であること' do
+        default_bread = build(:default_bread)
+        expect(default_bread).to be_valid
+      end
+    end
+  end
 end
