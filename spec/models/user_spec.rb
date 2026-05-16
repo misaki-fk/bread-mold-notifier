41d2ac8a5cec6493f676092b3f2d1bd2c288a6b7
深山海咲 <fukayama.misaki@gmail.com>
2026-05-16 15:10:33 +0900

    test(model): add User validations spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { build(:user) }
+  describe 'バリデーション' do
+    context 'email が空のとき' do
+      it '無効である' do
+        user = build(:user, email: '')
+        expect(user).to be_invalid
+      end
+    end
 
-  it "正常にユーザーを作成できる" do
-    expect(user).to be_valid
+    context 'password が空のとき' do
+      it '無効である' do
+        user = build(:user, password: '')
+        expect(user).to be_invalid
+      end
+    end
+
+    context 'password が短すぎるとき' do
+      it '無効である' do
+        user = build(:user, password: 'abc')
+        expect(user).to be_invalid
+        # password:（6文字未満）は無効
+      end
+    end
+
+    context '必要な属性が揃っているとき' do
+      it '有効である' do
+        user = build(:user)
+        expect(user).to be_valid
+      end
+    end
   end
 end
-
-
