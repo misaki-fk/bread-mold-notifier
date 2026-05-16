33171eb0b00da6cf639cfe4bd77c6a537c55b312
深山海咲 <fukayama.misaki@gmail.com>
2026-05-16 11:47:20 +0900

    test(model): add Bread associations spec

--- spec/models/bread_spec.rb
+++ spec/models/bread_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Bread, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '関連' do
+    context 'bread_type が nil のとき' do
+      it '無効である' do
+        bread = build(:bread)
+        bread.bread_type = nil
+        expect(bread).to be_invalid
+      end
+    end
+
+    context 'group が nil のとき' do
+      it '無効である' do
+        bread = build(:bread)
+        bread.group = nil
+        expect(bread).to be_invalid
+      end
+    end
+
+    context '関連が全部揃っているとき' do
+      it '有効である' do
+        bread = build(:bread)
+        expect(bread).to be_valid
+      end
+    end
+  end
 end
