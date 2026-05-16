47d9d0ec957d91d8d6c8e8b739d024a513e4d0da
深山海咲 <fukayama.misaki@gmail.com>
2026-05-16 12:09:47 +0900

    test(model): add Bread#days_until_expiration spec

--- spec/models/bread_spec.rb
+++ spec/models/bread_spec.rb
@@ -25,4 +25,71 @@ RSpec.describe Bread, type: :model do
       end
     end
   end
+
+  describe 'バリデーション' do
+    context 'expiration_date が nil のとき' do
+      it '無効である' do
+        bread = build(:bread, expiration_date: nil)
+        expect(bread).to be_invalid
+      end
+    end
+
+    context 'total_count が nil のとき' do
+      it '無効である' do
+        bread = build(:bread, total_count: nil)
+        expect(bread).to be_invalid
+      end
+    end
+
+    context 'daily_consumption が nil のとき' do
+      it '無効である' do
+        bread = build(:bread, daily_consumption: nil)
+        expect(bread).to be_invalid
+      end
+    end
+
+    context 'daily_consumption が 0 のとき' do
+      it '無効である' do
+        bread = build(:bread, daily_consumption: 0)
+        expect(bread).to be_invalid
+      end
+    end
+
+    context 'daily_consumption が小数のとき' do
+      it '無効である' do
+        bread = build(:bread, daily_consumption: 1.5)
+        expect(bread).to be_invalid
+      end
+    end
+  end
+
+  describe '#days_until_expiration' do
+    context 'expiration_date が nil のとき' do
+      it 'nil を返す' do
+        bread = build(:bread, expiration_date: nil)
+        expect(bread.days_until_expiration).to be_nil
+      end
+    end
+
+    context 'expiration_date が今日のとき' do
+      it '0 を返す' do
+        bread = build(:bread, expiration_date: Time.zone.today)
+        expect(bread.days_until_expiration).to eq(0)
+      end
+    end
+
+    context 'expiration_date が3日後のとき' do
+      it '3 を返す' do
+        bread = build(:bread, expiration_date: Time.zone.today + 3)
+        expect(bread.days_until_expiration).to eq(3)
+      end
+    end
+
+    context 'expiration_date が昨日のとき' do
+      it '-1 を返す' do
+        bread = build(:bread, expiration_date: Time.zone.today - 1)
+        expect(bread.days_until_expiration).to eq(-1)
+      end
+    end
+  end
 end
